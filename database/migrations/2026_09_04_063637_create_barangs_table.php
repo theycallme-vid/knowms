d9df840396d344cbbfc25d5e8dd0cdb83fe71f3a
David <davidgracelleo.290905@gmail.com>
2026-09-04 13:49:31 +0700

    membuatdatabase kategoris dan barangs + sudah terhubung satu sama lain

---
+++ database/migrations/2026_09_04_063637_create_barangs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barangs', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama', 100);
+            $table->decimal('harga', total: 9, places: 2);
+            $table->integer('stok')->default('0');
+            $table->unsignedBigInteger('kategori_id'); //membuat field kategori_id sebagai foreign key
+            $table->foreign('kategori_id')->references('id')->on('kategoris'); //membuat relasi one to many antara barang dan kategori
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barangs');
+    }
+};
